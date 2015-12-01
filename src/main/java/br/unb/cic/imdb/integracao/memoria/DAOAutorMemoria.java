32268ebb575267373f97fdc8cc2eebd9f8557544
LucasRez <macedolucas@hotmail.com>
2015-12-01 17:03:15 -0200

    Adicionados os DAOs ausentes

---
+++ src/main/java/br/unb/cic/imdb/integracao/memoria/DAOAutorMemoria.java
@@ -0,0 +1,37 @@
+package br.unb.cic.imdb.integracao.memoria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.unb.cic.imdb.integracao.DAOAutor;
+import br.unb.cic.imdb.negocio.Autor;
+
+public class DAOAutorMemoria implements DAOAutor {
+	private List<Autor> autores;
+	
+	public DAOAutorMemoria() {
+		autores =  new ArrayList<>();
+	}
+
+	@Override
+	public void salvar(Autor autor) {
+		autores.add(autor);
+		
+	}
+
+	@Override
+	public List<Autor> recuperaTodos() {
+		return autores;
+	}
+
+	@Override
+	public Autor recuperaPorNome(String nome) {
+		for (Autor autor : autores){
+			if (autor.getNome().equals(nome)) {
+				return autor;
+			}
+		}
+		return null;
+	}
+
+}
